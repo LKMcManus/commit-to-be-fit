df3b0bba30f4f02099e359e8bf97b33932985e2a
mastersinsttech <davidrobertcurtis@gmail.com>
2015-10-22 21:47:05 -0500

    quiz for mmod1

---
+++ scripts/quizMod1.js
@@ -0,0 +1,36 @@
+function processQuiz() 
+{  
+	var q1 = document.forms.quiz2.question1;   
+	var score=0;
+	
+	if( q1[ 0 ].checked )  
+		{   
+		score = score + 1;
+		} 
+	
+	var q2 = document.forms.quiz2.question2; 
+	
+	if( q2[ 2 ].checked )  
+	{   
+		score = score + 1;
+	}
+	
+	var q3 = document.forms.quiz2.question3;   
+		
+	if( q3[ 0 ].checked )  
+		{   
+			score = score + 1;
+			} 
+	var q4 = document.forms.quiz2.question4;   
+		
+	if( q4[ 1 ].checked )  
+		{   
+			score = score + 1;
+			} 
+	var q5 = document.forms.quiz2.question5;   
+	
+	if( q5[ 2 ].checked )  
+	{   
+		score = score + 1;
+		} 
+		alert("You got " + score + " correct!");
